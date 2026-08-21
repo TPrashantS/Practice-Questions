9f82d7cd5113bb58e19937344c1c5d2ed89b0c47
Prashant Kumar <151383063+TPrashantS@users.noreply.github.com>
2026-08-21 19:54:28 +0530

    Time: 2 ms (99.34%), Space: 47.1 MB (41.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,37 @@
+//Brute Force
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] ans = new int[2];
+//         int start = 0;
+//         while(start<nums.length){
+//             int curr = start + 1;
+//             while(curr<nums.length){
+//                 if(nums[start]+nums[curr]==target){
+//                     ans[0] = start;
+//                     ans[1] = curr;
+//                     break;
+//                 }
+//                 curr++;
+//             }
+//             start++;
+//         }
+//         return ans;
+//     }
+// }
+
+//Optimised (HashMap)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        int start = 0;
-        int curr = 1;
-        while(start<nums.length){
-            while(curr<nums.length){
-                if(nums[start]+nums[curr]==target){
-                    ans[0] = start;
-                    ans[1] = curr;
-                    break;
-                }
-                curr++;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            int req = target - nums[i];
+            if(hm.containsKey(req)){
+                return new int[]{hm.get(req), i};
             }
-            start++;
-            curr = start +1;
+            hm.put(nums[i], i);
         }
-        return ans;
+        return new int[]{-1,-1};
     }
 }
